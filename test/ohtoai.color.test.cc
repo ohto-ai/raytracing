93ce3e777664af1412d959c0e5803181f021002a
Choo <zhu.thatboy@outlook.com>
2024-01-10 02:00:36 +0800

    fix test failure

--- test/ohtoai.color.test.cc
+++ test/ohtoai.color.test.cc
@@ -5,10 +5,10 @@ TEST_CASE("Verify Color endian") {
     using Color = ohtoai::color::Colorf;
     Color c(0x11223344);
 
-    REQUIRE(c.red() == 0x44);
-    REQUIRE(c.green() == 0x33);
-    REQUIRE(c.blue() == 0x22);
-    REQUIRE(c.alpha() == 0x11);
+    REQUIRE(c.red() == 0x11);
+    REQUIRE(c.green() == 0x22);
+    REQUIRE(c.blue() == 0x33);
+    REQUIRE(c.alpha() == 0x44);
 }
 
 TEST_CASE("Verify Color operator") {
@@ -24,7 +24,7 @@ TEST_CASE("Verify Color operator") {
     REQUIRE(c.green() == 0x66);
     REQUIRE(c.blue() == 0x77);
     REQUIRE(c.alpha() == 0x88);
-    REQUIRE(c.to_easyx_color() == 0x88776655);
+    REQUIRE(c.to_easyx_color() == 0x00776655);
     REQUIRE(c.to_rgba() == 0x55667788);
 }
 
@@ -32,19 +32,19 @@ TEST_CASE("Verify Color::rgb, Color::rgba") {
     using Color = ohtoai::color::Colorf;
     Color c;
 
-    c = Color::rgb(0x11, 0x22, 0x33);
+    c = Color::from_rgb(0x11, 0x22, 0x33);
     REQUIRE(c.red() == 0x11);
     REQUIRE(c.green() == 0x22);
     REQUIRE(c.blue() == 0x33);
-    REQUIRE(c.alpha() == 0x00);
+    REQUIRE(c.alpha() == 0xff);
     REQUIRE(c.to_easyx_color() == 0x00332211);
 
-    c = Color::rgba(0x11, 0x22, 0x33, 0x44);
+    c = Color::from_rgba(0x11, 0x22, 0x33, 0x44);
     REQUIRE(c.red() == 0x11);
     REQUIRE(c.green() == 0x22);
     REQUIRE(c.blue() == 0x33);
     REQUIRE(c.alpha() == 0x44);
-    REQUIRE(c.to_easyx_color() == 0x44332211);
+    REQUIRE(c.to_easyx_color() == 0x00332211);
 }
 
 TEST_CASE("Verify Color operator Vector3") {
@@ -80,7 +80,7 @@ TEST_CASE("Verify Color operator Vector") {
     REQUIRE(c.green() == 2);
     REQUIRE(c.blue() == 3);
     REQUIRE(c.alpha() == 4);
-    REQUIRE(c.to_easyx_color() == 0x04030201);
+    REQUIRE(c.to_easyx_color() == 0x00030201);
 
     c = ohtoai::math::createVector(5, 6, 7);
     REQUIRE(c.red() == 5);
@@ -94,6 +94,6 @@ TEST_CASE("Verify Color operator Vector") {
     REQUIRE(c.green() == 12);
     REQUIRE(c.blue() == 13);
     REQUIRE(c.alpha() == 14);
-    REQUIRE(c.to_easyx_color() == 0x0E0D0C0B);
+    REQUIRE(c.to_easyx_color() == 0x000D0C0B);
 }
 
